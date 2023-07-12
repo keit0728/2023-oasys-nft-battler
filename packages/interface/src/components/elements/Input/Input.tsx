16eaed8aad32daddf6ffef036553db3188fd73e5
keit0728 <keit0728.job@gmail.com>
2023-07-13 00:22:12 +0900

    reademe修正 #25

--- packages/interface/src/components/elements/Input/Input.tsx
+++ packages/interface/src/components/elements/Input/Input.tsx
@@ -4,8 +4,10 @@ import clsx from "clsx";
 
 export type InputProps = {
   value?: string;
+  type?: string;
   placeholder?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
 } & BaseProps;
 
 /**
@@ -14,16 +16,20 @@ export type InputProps = {
  */
 export const Input = ({
   className,
+  type = "text",
   value,
   placeholder,
   onChange,
+  onBlur,
 }: InputProps) => {
   return (
     <input
       className={clsx(className, "p-[10px]", "bg-inputColor", "rounded-lg")}
+      type={type}
       value={value}
       placeholder={placeholder}
       onChange={onChange}
+      onBlur={onBlur}
     />
   );
 };
--- packages/interface/src/features/battle/components/MaxParticipantCountInput/MaxParticipantCountInput.tsx
+++ packages/interface/src/features/battle/components/MaxParticipantCountInput/MaxParticipantCountInput.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent } from "react";
 import { Input } from "@/components/elements/Input";
 import { inputMaxParticipantCountState } from "@/stores/inputMaxParticipantCountState";
 import { BaseProps } from "@/types/BaseProps";
+import { isNumber } from "@/utils/util";
 import clsx from "clsx";
 import { useRecoilState } from "recoil";
 
@@ -17,12 +18,17 @@ export const MaxParticipantCountInput = ({
   const [input, setInput] = useRecoilState(inputMaxParticipantCountState);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!isNumber(event.target.value)) {
+      setInput(0);
+      return;
+    }
     setInput(Number(event.target.value));
   };
 
   return (
     <Input
       className={clsx(className)}
+      type="number"
       value={input.toString()}
       placeholder="参加人数を入力"
       onChange={handleInputChange}
